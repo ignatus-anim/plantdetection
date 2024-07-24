a33d0094582acb00eca39fe63aa6499102071d43
Ignatus <ignatusa3@gmail.com>
2024-07-24 03:10:03 +0000

    fix errors

--- main.py
+++ main.py
@@ -82,6 +82,6 @@ async def predict(crop: Annotated[str, Form()], file: UploadFile = File(...)):
     }
 
 if __name__ == "__main__":
-    # uvicorn.run(app, host="localhost", port=8000)
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # port = int(os.environ.get("PORT", 8000))
+    # uvicorn.run(app, host="0.0.0.0", port=port)
