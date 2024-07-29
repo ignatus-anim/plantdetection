5bf5f22b5344d52139f12552e5db1e4cd5671e63
Ignatus <ignatusa3@gmail.com>
2024-07-29 19:54:38 +0000

    fix: set port to 10000 for render

--- main.py
+++ main.py
@@ -87,4 +87,4 @@ async def predict(crop: Annotated[str, Form()], file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
